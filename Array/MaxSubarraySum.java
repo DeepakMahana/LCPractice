9bdcad867fa549cc802f7594cff0f1e3352547bf
DEEPAK MAHANA <dmahana@falabella.cl>
2022-03-26 19:53:16 +0530

    NextPermutation & MaxSubSum

---
+++ Array/MaxSubarraySum.java
@@ -0,0 +1,37 @@
+package Array;
+
+public class MaxSubarraySum {
+
+    // TC - O(N), SC - 0(1)
+    public static int maxSubarraySum(int[] arr){
+        int maxSum = arr[0];
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+            if(sum > maxSum) maxSum = sum;
+            if(sum < 0) sum = 0;
+        }
+        return maxSum;
+    }
+    
+    // TC - O(N^2), SC - 0(1)
+    // public static int maxSubarraySum(int[] arr){
+    //     int maxSum = arr[0];
+    //     for(int i=0; i<arr.length; i++){
+    //         int sum = 0;
+    //         for(int j=i; j<arr.length; j++){
+    //             sum += arr[j];
+    //             maxSum = Math.max(sum, maxSum);
+    //         }
+    //     }
+    //     return maxSum;
+    // }
+
+    public static void main(String[] args) {
+        // int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        // int[] arr = {5,4,-1,7,8};
+        int[] arr = {-2,-1};
+        System.out.println(maxSubarraySum(arr));
+    }
+    
+}
---
+++ Array/NextPermutation.java
@@ -0,0 +1,50 @@
+// Problem Link :- https://leetcode.com/problems/next-permutation/
+
+package Array;
+
+import java.util.Arrays;
+
+public class NextPermutation {
+
+    public static int[] nextPermutation(int arr[]){
+
+        if(arr == null || arr.length <= 1) return arr;
+
+        // Traverse from back and find the breakpoint
+        int i = arr.length - 2;
+        while( i >= 0 && arr[i] >= arr[i+1] ) i--; 
+
+        // Find the first bigger element than the breakpoint value
+        if(i>=0){
+            int j = arr.length - 1;
+            while(arr[j] <= arr[i]) j--;
+            // Swap the breakpoint value with the element greater than it
+            swap(arr, i, j);
+        }
+        
+        // If no breakpoint found just reverse the array
+        reverse(arr, i+1, arr.length-1);
+        
+        return arr;
+    }
+
+    // Reverse array
+    public static void reverse(int[] arr, int i, int j){
+        while (i<j) swap(arr, i++, j--);
+    }
+
+    // Swap the elements
+    public static void swap(int []arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        // int[] arr = {1,3,5,4,2};
+        // int[] arr = {1,1,5};
+        int[] arr = {3,2,1};
+        System.out.println(Arrays.toString(nextPermutation(arr)));
+    }
+    
+}
