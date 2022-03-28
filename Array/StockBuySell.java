643878ea552a8f3cecc5a046d3c9f934335e7ce9
DEEPAK MAHANA <dmahana@falabella.cl>
2022-03-28 19:38:55 +0530

    StockBuySell

---
+++ Array/StockBuySell.java
@@ -0,0 +1,23 @@
+// Problem Link :- https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+package Array;
+
+public class StockBuySell {
+
+    public static int maxProfit(int[] arr){
+        int maxProfit = 0;
+        int minimum = Integer.MAX_VALUE;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] < minimum) minimum = arr[i];
+            maxProfit = Math.max(maxProfit, arr[i] - minimum);
+        }
+        return maxProfit;
+    }
+    
+    public static void main(String[] args) {
+        // int[] arr = {7,1,5,3,6,4};
+        int[] arr = {7,6,4,3,1};
+        System.out.println(maxProfit(arr));
+    }
+
+}
